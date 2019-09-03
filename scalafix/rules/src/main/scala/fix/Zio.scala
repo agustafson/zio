696547a15b53b1419e2a0b8898cda2e5a138beb3
Andrew Gustafson <gus4000@gmail.com>
2019-09-03 22:15:12 +0100

    Replace IO with Task, pure & raiseError

--- scalafix/rules/src/main/scala/fix/Zio.scala
+++ scalafix/rules/src/main/scala/fix/Zio.scala
@@ -2,15 +2,20 @@ package fix
 
 import scalafix.v1._
 import scala.meta._
-import scalafix.lint.LintSeverity
 
 class Zio extends SemanticRule("Zio") {
 
   override def fix(implicit doc: SemanticDocument): Patch = {
-    println("Tree.syntax: " + doc.tree.syntax)
-    println("Tree.structure: " + doc.tree.structure)
-    println("Tree.structureLabeled: " + doc.tree.structureLabeled)
-    Patch.empty
+    Patch.replaceSymbols(
+      "cats/effect/IO" -> "zio/Task"
+    ) ++ doc.tree.collect {
+      case pureMatcher(Term.Apply(Term.Select(_, s), _)) =>
+        Patch.replaceTree(s, "succeed")
+      case raiseErrorMatcher(Term.Apply(Term.Select(_, s), _)) =>
+        Patch.replaceTree(s, "fail")
+    } + Patch.removeImportee(Importee.Name(Name("cats/effect/IO")))
   }
 
+  val pureMatcher = SymbolMatcher.normalized("cats/effect/IO#pure.")
+  val raiseErrorMatcher = SymbolMatcher.normalized("cats/effect/IO#raiseError.")
 }
